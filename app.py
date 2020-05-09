7a110a8a0bd6d6b93e5e45f9bd977c68914c8d6e
Abha Mutalik <abhamutalik@gmail.com>
2020-05-08 20:07:15 -0400

    initial dashboard added, dynamically updating side menu

--- app.py
+++ app.py
@@ -5,11 +5,23 @@ import numpy as np
 import json
 import pandas as pd
 
+df = pd.read_csv("data/Influenza_NY.csv")
+returned_data = {}
+
 app = Flask(__name__)
+
+def getAll_counties():
+    all_counties = list(df["County"].unique())
+    return all_counties
 
 @app.route('/', methods = ["GET"])
 def index():
-    return render_template("index.html")
+    all_data = df.to_dict(orient='records')
+    all_data = json.dumps(all_data, indent=2)
+    all_counties = getAll_counties()
+    returned_data["all_data"] = all_data
+    returned_data["all_counties"] = json.dumps(all_counties)
+    return render_template("index.html", returned_data = returned_data)
     
 if __name__ == '__main__':
     app.run(debug=True)
